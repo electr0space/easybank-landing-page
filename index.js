21be28f677366c4c0820aa1288aaee61c709872d
Alina <101723261+electr0space@users.noreply.github.com>
2024-03-24 17:05:47 +0200

    Update carousel

--- index.js
+++ index.js
@@ -8,16 +8,13 @@ const blogCards = document.querySelector(".blog__cards"); // carousel
 const firstCard = blogCards.querySelectorAll(".blog__card")[0];
 const arrows = document.querySelectorAll(".blog i");
 
-// let firstCardWidth = firstCard.clientWidth + 18; // here in px, in css - rem
-
 // Get the actual margin value (might be different on different screens)
 let marginRight = parseInt(getComputedStyle(firstCard).marginRight);
 
-// Calculate total spacing (gap + margin-right)
-let totalSpacing = parseInt(getComputedStyle(blogCards).gap) + marginRight;
-
 let cardWidth = firstCard.clientWidth;
-let scrollDistance = cardWidth + totalSpacing;
+let scrollDistance = cardWidth + marginRight;
+
+// Navbar hide on scroll down, appear on scroll up
 
 window.addEventListener("scroll", () => {
     const currentScrollPos = window.pageYOffset;
@@ -36,23 +33,16 @@ window.addEventListener("scroll", () => {
 
 // Slider for blog posts 
 
+arrows.forEach((arrow) => {
+    arrow.addEventListener("click", () => {
+        blogCards.scrollLeft += arrow.id === "left" ? -scrollDistance : scrollDistance;
+    })
+})
+
 const showArrows = () => {
-    arrows[0].style.display = blogCards.scrollLeft === 0 ? "none" : "block";
     let scrollWidth = blogCards.scrollWidth - blogCards.offsetWidth;
+    arrows[0].style.display = blogCards.scrollLeft === 0 ? "none" : "block";
     arrows[1].style.display = blogCards.scrollLeft === scrollWidth ? "none" : "block";
-  }
+}
   
 blogCards.addEventListener("scroll", showArrows);
-
-arrows.forEach((arrow) => {
-arrow.addEventListener("click", () => {
-    // let firstCardWidth = firstCard.offsetWidth;
-    blogCards.scrollLeft += arrow.id === "left" ? -scrollDistance : scrollDistance;
-})
-})
-
-// Scroll: seems like there is a thin line that is moving on button click. I might need to rewrite everything: set the starting point from the start at width of the first image
-
-
-
-// What if box-shadow replace with padding-bottom for navbar to take reemaining height of the page and place there background linear-gradient?
